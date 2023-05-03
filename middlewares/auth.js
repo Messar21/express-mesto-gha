e4edb35faff7b9a47cf6e9ab24fe5eb196b77804
Anton Geras <jutsio@yandex.ru>
2023-05-03 03:28:48 +0300

    Исправить централизованную обработку ошибок v2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,23 @@ const Unauthorised = require('../utils/errors/unauth-error');
 const SECRET = 'mosthiddensecretofallsecrets';
 
 const auth = async (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Unauthorised('Необходима авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
-  let payload;
   try {
-    payload = await jwt.verify(token, SECRET);
+    const { authorization } = req.headers;
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      throw new Unauthorised('Необходима авторизация');
+    }
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+    try {
+      payload = await jwt.verify(token, SECRET);
+    } catch (err) {
+      next(new Unauthorised('Необходима авторизация'));
+    }
+    req.user = payload;
+    next();
   } catch (err) {
-    next(new Unauthorised('Необходима авторизация'));
+    next(err);
   }
-  req.user = payload;
-  next();
 };
 
 module.exports = { auth };
